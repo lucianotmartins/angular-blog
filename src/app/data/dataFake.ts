654e7dcf2b95fd16f4e455c442d4a0a6aedca8c9
Luciano Martins <lucianotmartins.93@gmail.com>
2023-10-13 11:18:42 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"NOVO HOMEM DE FERRO EM 3D",
+    "description": "Marvel Anuncia novo Home de Ferro",
+    "photoCover": "https://is5-ssl.mzstatic.com/image/thumb/toO8_a7YKPIpfUgyJ7MnYg/1200x675mf.jpg"
+  },
+  {
+    "id":"2",
+    "title":"Nova Série anunciada do Disney +",
+    "description": "Disney Anuncia nova serie",
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id":"3",
+    "title":"Novo filme do Pantera Negra lançado em breve",
+    "description": "Marvel Anuncia novo Pantera Negra",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRXWGyKOjzA0Mu_tmlXvX2uuHzIp7d3A08qQ&usqp=CAU"
+  },
+  {
+    "id":"4",
+    "title":"Novo filme dos x-men está em pós produção, afirma diretor.",
+    "description": "DC Anuncia novo filme do x-men",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhC3VUc5r1l_3g7E2i5MjKyBeLtKPLTFUHye5USsDo2BIupPBlWtzzCFcfx-bdbEUqzgg&usqp=CAU"
+  }
+]
